b55540a805f1c2b3cf38482e8529196dcb476db9
Bani Montoya <banimontoya@gmail.com>
2015-01-10 21:09:58 -0500

    index

--- django_project/urls.py
+++ django_project/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'central.views.home', name='home'),
+    url(r'^$', 'central.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^gestor/', include(admin.site.urls)),
