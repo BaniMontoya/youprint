fedfb187e985efb25af89ef713f43a8fb624137e
Bani Montoya <banimontoya@gmail.com>
2015-01-10 21:50:38 -0500

    email

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = '2mK8UN5feTOMM9y4cxBRpEYPfDK2LYjteLd1woxkIQhKinQw4U'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ACCOUNT_ACTIVATION_DAYS=7
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'mail.imetafora.com'
 
-EMAIL_HOST_USER = 'bmksoporte@gmail.com'
+EMAIL_HOST_USER = 'bmk@imetafora.com'
 
 EMAIL_HOST_PASSWORD = 'Amor.789'
 
